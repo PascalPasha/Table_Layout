99e5694ab20061c7f24dbc2cd19e6d02dd60512e
PascalPasha <testiing1212@gmail.com>
2021-03-07 21:06:34 +0700

    Membuat code agar bisa show detail informasi

--- app/src/main/java/com/example/table_layout/MainActivity.java
+++ app/src/main/java/com/example/table_layout/MainActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean table_lyt = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Table(View view) {
+        TableLayout table = findViewById(R.id.table);
+        Button btndetail = findViewById(R.id.btndetail);
 
+        table.setColumnCollapsed(1, table_lyt);
+        table.setColumnCollapsed(2, table_lyt);
+
+        if (table_lyt) {
+            table_lyt = false;
+            btndetail.setText("Show Detail");
+        }
+        else {
+            table_lyt = true;
+            btndetail.setText("Hide` Detail");
+        }
     }
 }
